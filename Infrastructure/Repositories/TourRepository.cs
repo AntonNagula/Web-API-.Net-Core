7cf0f7b999f6a9eff0c46c791ebaafa2c51905c4
Anton <nagula.anton@mail.ru>
2020-05-02 15:50:42 +0300

    add(functionality)

--- Infrastructure/Repositories/TourRepository.cs
+++ Infrastructure/Repositories/TourRepository.cs
@@ -67,15 +67,15 @@ namespace Data.Repositories
             return Tours.ToList().FirstOrDefault(x => x.TourId == id.ToString());
         }
         public IEnumerable<Tour> GetChoisenTours(ChoisenCriterials choisenCriterials)
-        {
-            DateTime start = Convert.ToDateTime(choisenCriterials.StartDate + " 00:00:00");
-            DateTime end = Convert.ToDateTime(choisenCriterials.EndDate + " 23:59:59");
-            int countryId = Convert.ToInt32(choisenCriterials.CountryId);
+        {            
+            DateTime start = choisenCriterials.StartDate != "" ? Convert.ToDateTime(choisenCriterials.StartDate + " 00:00:00") : Convert.ToDateTime("01.01.2020" + " 00:00:00");
+            DateTime end = choisenCriterials.EndDate != "" ? Convert.ToDateTime(choisenCriterials.EndDate + " 23:59:59") : Convert.ToDateTime("01.01.2100" + " 23:59:59");
+            int countryId = choisenCriterials.CountryId != "" ? Convert.ToInt32(choisenCriterials.CountryId) : 0;
             var tours = from tour in database.Tours
                         join country in database.Countries on tour.CountryId equals country.CountryId
                         join city in database.Cities on tour.CityId equals city.CityId
                         join hotel in database.Hotels on tour.HotelId equals hotel.HotelId
-                        where tour.EndQuantity > 0 && tour.StartDate > DateTime.Today && tour.StartDate >= start && tour.StartDate <= end && tour.CountryId == countryId 
+                        where tour.EndQuantity > 0 && tour.StartDate > DateTime.Today && tour.StartDate >= start && tour.StartDate <= end && (countryId != 0 ? tour.CountryId == countryId : true)
                         select new Tour
                         {
                             CityId = city.CityId.ToString(),
